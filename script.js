029e19dba7eab491bb06e03b3740b15b81cb1ee9
olivier villevieille <oliviervillevieille@hotmail.fr>
2022-05-14 11:01:05 +0200

    modif all fichiers

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ const header = document.querySelector("header");
 const title_span = document.querySelectorAll(".left h1 span");
 const p = document.querySelector(".left p");
 const a = document.querySelector(".left a");
-const img = document.querySelector(".image_bird");
+const img = document.querySelector(".image_perso");
+const footer = document.querySelector(".footer");
 
 window.addEventListener("load", () => {
   const TL = gsap.timeline({ paused: true });
@@ -21,7 +22,8 @@ window.addEventListener("load", () => {
     .staggerFrom(img, 1, { x: 1000, opacity: 0, ease: "power2.out" }, 0.6)
     .staggerFrom(title_span, 1, { opacity: 0, ease: "power1.out" }, 0.5)
     .staggerFrom(p, 1, { opacity: 0, ease: "power1.out" }, 0.5)
-    .staggerFrom(a, 1, { opacity: 0, ease: "power2.out" }, 0.6);
+    .staggerFrom(a, 1, { opacity: 0, ease: "power2.out" }, 0.6)
+    .staggerFrom(footer, 1, { opacity: 0, ease: "power2.out" }, 0.7);
 
   TL.play();
 });
